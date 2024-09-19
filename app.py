ee9f983eea2dcf74e9c688c7a158cf0044215ad0
isaaclefebre16 <135345043+isaaclefebre16@users.noreply.github.com>
2024-09-19 09:27:47 -0300

    Aula 19-09-2024

--- app.py
+++ app.py
@@ -14,21 +14,34 @@ print('2. Listar Alunos')
 print('3. Ativar Aluno')
 print('4. Sair da aplicação')
 
-opcao_escolhida = int(input('Escolha uma opção: ') )
-print('você escolheu a opção: ', opcao_escolhida)
+def escolhe_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: ') )
+        print('você escolheu a opção: ', opcao_escolhida)
 
-def finalizar_programa():
-    os.sistem('cls')
-    print('finalizando programa')
+        if opcao_escolhida == 1:
+            print('Cadastrar Aluno')
+        elif opcao_escolhida == 2:
+            print('Listar Aluno')
+        elif opcao_escolhida == 3:
+            print('Ativar/desativar Aluno')
+        elif opcao_escolhida == 4:
+            finalizar_programa()
+        else:
+            opcao_invalida()
 
-if opcao_escolhida == 1:
-    print('Cadastrar Aluno')
-elif opcao_escolhida == 2:
-    print('Listar Aluno')
-elif opcao_escolhida ==3:
-    print('Ativar/desativar Aluno')
-else:
-    finalizar_programa()
+except:
+    opcao_invalida()
+
+def cadastrar_alunos():
+    os.system('cls')
+    print('cadastrando aluno... ')
+    nome_nadador = input('Digite o nome do aluno: ')
+    alunos.append(nome_aluno)
+    print(f'(nome_aluno) foi adicionado(a) aos alunos de Foz do Iguaçu')
+    input('Digite qualquer tecla para voltar')
+    main()
+
 
 def main():
     mostra_titulo()
